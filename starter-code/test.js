266942b360a790304db3897359f171dffe3efc69
lenie chow <leniechow@gmail.com>
2016-09-29 11:33:20 +0800

    timer works

--- starter-code/test.js
+++ starter-code/test.js
@@ -1,15 +1,17 @@
-function init() {
+ function init() {
 
-var seconds =0;
-var timerId =0;
+ var seconds =0;
+ var timerId =0;
 
 
+// step 1 - create selectors - for buttons
+var body = document.querySelector('body');
 var resetTimer = document.querySelector('#reset');
 var startTimer= document.querySelector('#start');
 var pauseTimer =document.querySelector('#pause');
 var timer = document.querySelector('#timer');
 
-
+// Step2 Set up event listeners for click
 
 startTimer.addEventListener("click" , startTheTimer);
 
@@ -18,25 +20,24 @@ timer.innerHTML = "Time Elapsed : " + seconds;
 timerId= setInterval (updateTime ,1000);
 
 }
- 
+
 
 pauseTimer.addEventListener("click" , pauseTheTimer);
 
 function pauseTheTimer () {
- clearInterval(timerId);
- timer.innerHTML = "Time Elapsed : " + seconds;
- timerId = 0;
-
+  clearInterval(timerId);
+  timer.innerHTML = "Time Elapsed : " + seconds;
+  timerId = 0;
 
 }
 
 resetTimer.addEventListener("click", resetTheTimer);
 
 function resetTheTimer () {
- clearInterval(timerId);
- timer.innerHTML = "Stop Watch";
- timerId = 0;
- seconds = 0;
+  clearInterval(timerId);
+  timer.innerHTML = "Stop Watch";
+  timerId = 0;
+  seconds = 0;
 
 }
 
